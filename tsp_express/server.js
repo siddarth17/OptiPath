bc7ac504a9f350e4101778ff6ae10947bd24ea73
Siddarth Rudraraju <sr88018@usc.edu>
2024-08-06 17:05:26 +0530

    h

--- tsp_express/server.js
+++ tsp_express/server.js
@@ -189,9 +189,11 @@ app.post('/register', async (req, res) => {
       await user.save();
       res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
-      res.status(500).json({ error: 'Registration failed' });
+      console.error('Registration failed:', error); // Ensure you log the error
+      res.status(500).json({ error: 'Registration failed', message: error.message });
     }
-  });
+});
+
   
   // User login
 app.post('/login', async (req, res) => {
